85e5061b83a677ca9abe25b276213197aaa1972c
dishant negi <negidishant5@gmail.com>
2024-12-04 18:48:38 +0530

    last2

--- Frontend Rent Cars/app/about/page.tsx
+++ Frontend Rent Cars/app/about/page.tsx
@@ -3,10 +3,8 @@
 import React from 'react'
 import Why from '../components/Why'
 import FAQSection from '../components/FAQSection'
-import Footer from '../components/Footer'
 import Banner from '../components/Banner'
 import CardsSection from '../components/AboutPageCards'
-import TestimonialsCarousel from '../components/TestimonalsCarousel'
 import Features from '../components/Features'
 
 const page = () => {
